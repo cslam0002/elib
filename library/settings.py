01329aacfe593c8de9e48f8d8508716ba5dd7fa1
shing <mylesson0002@gmail.com>
2025-06-13 15:37:51 +0800

    access to postgres libdb

--- library/settings.py
+++ library/settings.py
@@ -79,8 +79,13 @@ WSGI_APPLICATION = 'library.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'libdb',
+        'USER': 'postgres',
+        'PASSWORD' : os.getenv('DB_PASSWORD'),
+        'HOST' : 'localhost',
     }
 }
 
