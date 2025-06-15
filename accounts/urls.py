22cd8c3b60f0f7ef3c3b9909d6c25f64a6398fc5
shing <mylession002@gmail.com>
2025-06-15 15:59:15 +0800

    create user account and provide login/logout service

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,10 +4,13 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
+    path('register', views.register, name='register'),
+
+
     path('edit', views.edit, name='edit'),
     path('listing', views.listing, name='listing'),
-    path('login', views.login, name='login'),
-    path('register', views.register, name='register'),
     path('reset', views.reset, name='reset'),
     path('search', views.search, name='search'),
     path('update', views.update, name='update'),
