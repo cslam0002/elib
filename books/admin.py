2d991cd95500e12f15005e2254264c81850cd803
shing <mylesson0002@gmail.com>
2025-06-20 11:26:56 +0800

    register book & record admin

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import Book
+
 # Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'isbn', 'year', 'total', 
+                    'stock', 'language', 'category', 'cover_url')
+    list_display_links = 'title', 
+    list_editable = 'total',
+    search_fields = ('title', 'author', 'isbn', 'category')
+    list_per_page = 25
+
+admin.site.register(Book, BookAdmin)
--- records/admin.py
+++ records/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import BorrowRecord
+
 # Register your models here.
+
+
+class BorrowRecordAdmin(admin.ModelAdmin):
+    list_display = 'user', 'book', 'status', 'due_date'
+    list_display_links = 'user', 'book', 'due_date'
+    list_editable = 'status',
+    search_fields = 'user', 
+    list_per_page = 25
+
+admin.site.register(BorrowRecord, BorrowRecordAdmin)
+
