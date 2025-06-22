08591df1eae4dedde768a48516a73af217563d74
shing <mylesson0002@gmail.com>
2025-06-22 23:33:42 +0800

    media path configuration

--- library/urls.py
+++ library/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path
 
 from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('pages.urls', namespace='pages')),
@@ -27,3 +29,6 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
 ]
+
+# register media url
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
